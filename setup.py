2be5c43e49ee9d0d7d5d5062a888328a05af3dd5
Jean-Paul Wenger <jpaul.wenger@gmail.com>
2025-09-16 11:56:37 +0200

    publishing github action

--- setup.py
+++ setup.py
@@ -3,16 +3,23 @@ from setuptools import setup
 
 setup(
     name="nasow",
-    version="0.0.1",
+    version="1.0.0",
     packages=[
       "nasow"
     ],
     include_package_data=True,
-    description="TODO",
+    description="Nasow is a simple small python script which synchronizes DNS servers for WSL from Windows.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     author="Jean-Paul Wenger",
     author_email="jpaul.wenger@gmail.com",
-    url="https://github.com",
+    url="https://github.com/jpweng/nasow",
     install_requires=[
         "pathlib2",
     ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+    ]
 )
